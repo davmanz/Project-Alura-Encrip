b57aa277eef08b5e50f66f08faed9ee90489c884
david <davidjosemanzano@gmail.com>
2023-04-15 20:14:23 -0500

    Nueva actualizacion

--- script.js
+++
@@ -1,6 +0,0 @@
-const textarea = document.getElementById("comentarios");
-
-textarea.addEventListener("input", () => {
-  textarea.style.height = "auto";
-  textarea.style.height = `${textarea.scrollHeight}px`;
-});
